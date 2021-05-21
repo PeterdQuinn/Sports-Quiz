82ccc4be5c12bbeca88c64998aa78b0c9d016148
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-21 00:21:26 -0700

    final commit

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,35 @@
+
+var questions = [
+  {
+    title: "Who won the 2020 stanley cup finals?:",
+    choices: ["tampa bay", "Hornets", "rangers", "islanders"],
+    answer: "tampa bay"
+  },
+  {
+    title: "Who is the 2020 Nba Champs?",
+    choices: ["knicks", "lakers", "magic", "kings"],
+    answer: "lakers"
+  },
+  {
+    title: "who is the 2020 nfl champs?",
+    choices: [
+      "Tampa bay",
+      "Raiders",
+      "Rams",
+      "Patriots"
+    ],
+    answer: "Tampa bay"
+  },
+  {
+    title:
+      "Who is the 2020 mlb champs?",
+    choices: ["Dodgers", "Diamondbacks", "Angels", "Yanks"],
+    answer: "Dodgers"
+  },
+  {
+    title:
+      "who is the current mls champs?:",
+    choices: ["Columbus SC", "Seattle", "Toronto", "NY RED BULLS"],
+    answer: "Columbus SC"
+  }
+];
